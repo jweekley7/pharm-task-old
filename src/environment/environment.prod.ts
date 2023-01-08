a4c5aba5cd364f2f1b6939914ae176eef969c4a6
jweekley7 <justinweekley7@gmail.com>
2023-01-08 08:43:17 -0600

    firebase ui

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,3 +1,5 @@
+import firebase from 'firebase/compat/app';
+
 export const firebaseConfig = {
   apiKey: "AIzaSyA9tosrTFCF6AESWK6DHbLtrjLv1VEaz1o",
   authDomain: "ivroomduty-444a0.firebaseapp.com",
@@ -6,3 +8,29 @@ export const firebaseConfig = {
   messagingSenderId: "887113874529",
   appId: "1:887113874529:web:6143e183683c692f88be20"
 };
+
+// export const uiConfig = {
+//   //signInSuccessUrl: '<url-to-redirect-to-on-success>',
+//   signInOptions: [
+//     // {
+//     //   provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//     //   requireDisplayName: false
+//     // },
+//     // Leave the lines as is for the providers you want to offer your users.
+//     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//     // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+//     // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+//     // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+//     // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//     // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+//     // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+//   ],
+//   // tosUrl and privacyPolicyUrl accept either url string or a callback
+//   // function.
+//   // Terms of service url/callback.
+//   // tosUrl: '<your-tos-url>',
+//   // // Privacy policy url/callback.
+//   // privacyPolicyUrl: function() {
+//   //   window.location.assign('<your-privacy-policy-url>');
+//   // }
+// };
--- src/services/firebase-core.tsx
+++ src/services/firebase-core.tsx
@@ -2,6 +2,10 @@
 import { initializeApp, FirebaseApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { firebaseConfig } from "../environment/environment.prod";
+import { getAuth } from "firebase/auth";
+
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,3 +13,44 @@ import { firebaseConfig } from "../environment/environment.prod";
 // Initialize Firebase
 const app: FirebaseApp = initializeApp(firebaseConfig);
 export const DBApp = getFirestore(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+
+firebase.initializeApp(firebaseConfig);
+
+// Initialize the FirebaseUI Widget using Firebase.
+export const firebaseUi = new firebaseui.auth.AuthUI(firebase.auth());
+
+export const uiConfig = {
+  //signInSuccessUrl: '<url-to-redirect-to-on-success>',
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false
+    },
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  // tosUrl and privacyPolicyUrl accept either url string or a callback
+  // function.
+  // Terms of service url/callback.
+  // tosUrl: '<your-tos-url>',
+  // // Privacy policy url/callback.
+  // privacyPolicyUrl: function() {
+  //   window.location.assign('<your-privacy-policy-url>');
+  // }
+};
+
+// firebaseUi.start('#firebaseui-auth-container', {
+//   signInOptions: [
+//     firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//   ],
+//   // Other config options...
+// });
