08b6a26cf6ea9e8f7158949e95d188aeaa6d1130
jweekley7 <91277836+jweekley7@users.noreply.github.com>
2023-01-10 11:12:54 -0600

    Update facility.tsx

--- src/models/facility.tsx
+++ src/models/facility.tsx
@@ -1,7 +1,12 @@
+import { iChecklist } from './checklist.tsx';
+import { iUser } from './user.tsx';
+
 export interface iFacility {
   facilityId: string;
   facilityName: string;
   logOnID: string;
   logOnPassWord: string;
-  facilityAdmin: string[];
+  facilityAdmin: string[]; //string of emails or ids?
+  checklist: iChecklist[];
+  users: iUser[]
 }
