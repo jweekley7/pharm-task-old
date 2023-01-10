86d58b8f018bdbaad8baf7c5fd9af1f996bfc3f8
jweekley7 <91277836+jweekley7@users.noreply.github.com>
2023-01-10 12:49:49 -0600

    Update user.tsx

--- src/models/user.tsx
+++ src/models/user.tsx
@@ -1,7 +1,7 @@
 import { userRoles } from "../utils/constants";
 
 export interface iUser {
-  role: userRoles; //What if a user is admin for one place but not another?
+  role: userRoles; //QUESTION: What if a user is admin for one place but not another?
   userEmail: string;
   userId: string;
   fullName: string;
