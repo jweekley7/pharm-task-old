f2a1cf807ee9577bab9714c1d20663c493e01b7f
jweekley7 <justinweekley7@gmail.com>
2023-01-12 18:38:18 -0600

    main page

--- src/modules/main/App.tsx
+++ src/modules/main/App.tsx
@@ -3,10 +3,12 @@ import Home from '../facilityHome/home';
 import { useContext, useState } from 'react';
 import { FacilityContext } from '../../providers/facility-provider';
 import NewFacility from '../new-facility/new-facility';
-import BasicModal from '../../modals/basic-modal';
+import BasicModal from '../../components/modals/basic-modal';
 import { AuthContext } from '../../providers/auth-provider';
 import FacilityLogin from '../login/facility-login';
 import UserLogin from '../login/user-login';
+import Card from '@mui/material/Card';
+import { CardContent } from '@mui/material';
 
 
 export default function App() {
@@ -42,12 +44,48 @@ export default function App() {
       {currentFacility ? 
         <Home/> : 
         <div>
-          <div className="text-3xl font-bold underline">Welcome to checklist.</div>
-          <div>Existing Facility? Login <a className='cursor-pointer' onClick={() => showAccountLogInModal()}>here</a></div>
-          <div>Facility Admin? Login <a className='cursor-pointer' onClick={() => showFacilityAdminLoginModal()}>here</a></div>
-          <div>New Facility? Create Account <a className='cursor-pointer' onClick={() => showCreateFacilityAccountModal()}>
-              here
-            </a>
+          <div className="text-3xl font-bold underline">Welcome to PharmTask!</div>
+          <div className='flex mx-6 space-x-4 my-6'>
+            <Card 
+              sx={{ minWidth: 225 }}
+              onClick={() => showAccountLogInModal()}
+              className='cursor-pointer hover:transition hover:duration-300 hover:scale-110 text-center'
+            >
+              <CardContent>
+                <div>
+                  <p>Existing Facility?</p> 
+                  <p>Login <a className='cursor-pointer' onClick={() => showAccountLogInModal()}>here</a></p>
+                </div>
+              </CardContent>
+            </Card>
+            
+            <Card 
+              sx={{ minWidth: 225 }}
+              onClick={() => showFacilityAdminLoginModal()}
+              className='cursor-pointer hover:transition hover:duration-300 hover:scale-110 text-center'
+            >
+              <CardContent>
+                <div>
+                  <p>Facility Admin?</p>
+                  <p>Login <a className='cursor-pointer' onClick={() => showFacilityAdminLoginModal()}>here</a></p>
+                </div>
+              </CardContent>
+            </Card>
+
+            <Card               
+              sx={{ minWidth: 225 }}
+              onClick={() => showCreateFacilityAccountModal()}
+              className='cursor-pointer hover:transition hover:duration-300 hover:scale-110 text-center'
+            >
+              <CardContent>
+                <div>
+                  <p>New Facility?</p> 
+                  <p>Create Account 
+                    <a className='cursor-pointer' onClick={() => showCreateFacilityAccountModal()}> here</a>
+                  </p>
+              </div>
+              </CardContent>
+            </Card>
           </div>
           <div>
             {showLogInModal &&
